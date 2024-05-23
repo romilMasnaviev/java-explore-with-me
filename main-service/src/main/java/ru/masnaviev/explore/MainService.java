e3b7305953b51838645a49121e7c2dd471f8d368
romilMasnaviev <134401753+romilMasnaviev@users.noreply.github.com>
2024-05-23 17:11:35 +0300

    add structure; dependencies to stat-server

---
+++ main-service/src/main/java/ru/masnaviev/explore/MainService.java
@@ -0,0 +1,9 @@
+package ru.masnaviev.explore;
+
+public class MainService
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ stat/stat-client/src/main/java/ru/masnaviev/explore/App.java
@@ -0,0 +1,13 @@
+package ru.masnaviev.explore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
