26d472961cb099854400054379e2c36100dc9b71
romilMasnaviev <134401753+romilMasnaviev@users.noreply.github.com>
2024-07-01 22:33:21 +0300

    add order by hits

--- stat/stat-server/src/main/java/ru/masnaviev/explore/dao/StatEntityRepository.java
+++ stat/stat-server/src/main/java/ru/masnaviev/explore/dao/StatEntityRepository.java
@@ -11,37 +11,13 @@ import java.util.List;
 
 public interface StatEntityRepository extends JpaRepository<StatEntity, Integer> {
 
-//    @Query("select new ru.masnaviev.explore.dto.StatEntityGetResponse(s.app, s.uri, count(s)) " +
-//            "from StatEntity s " +
-//            "where s.timestamp > :start and s.timestamp < :end " +
-//            "and (:uris is null or s.uri in :uris) " +
-//            "and (:unique = false or s.ip in (select t.ip from StatEntity t " +
-//            "where t.timestamp > :start and t.timestamp < :end)) " +
-//            "group by s.app, s.uri")
-//    List<StatEntityGetResponse> getStatistics(@Param("start") LocalDateTime start,
-//                                              @Param("end") LocalDateTime end,
-//                                              @Param("uris") List<String> uris,
-//                                              @Param("unique") boolean unique);
-
-//    @Query("select new ru.masnaviev.explore.dto.StatEntityGetResponse(s.app, s.uri, count(s)) " +
-//            "from StatEntity s " +
-//            "where s.timestamp > :start and s.timestamp < :end " +
-//            "and (:uris is null or :uris = '[]' or s.uri in (:uris)) " +
-//            "and (:unique = false or s.ip in (select t.ip from StatEntity t " +
-//            "where t.timestamp > :start and t.timestamp < :end " +
-//            "group by t.ip)) " +
-//            "group by s.app, s.uri")
-//    List<StatEntityGetResponse> getStatistics(@Param("start") LocalDateTime start,
-//                                              @Param("end") LocalDateTime end,
-//                                              @Param("uris") List<String> uris,
-//                                              @Param("unique") boolean unique);
-
     @Query("select new ru.masnaviev.explore.dto.StatEntityGetResponse(s.app, s.uri, " +
             "count(distinct (case when :unique = true then s.ip else concat(s.uri, s.ip,s.timestamp) end))) " +
             "from StatEntity s " +
             "where s.timestamp > :start and s.timestamp < :end " +
             "and (:#{#uris == null || #uris.isEmpty()} = true OR s.uri IN :uris) " +
-            "group by s.app, s.uri")
+            "group by s.app, s.uri " +
+            "order by count(distinct (case when :unique = true then s.ip else concat(s.uri, s.ip,s.timestamp) end)) desc ")
     List<StatEntityGetResponse> getStatistics(@Param("start") LocalDateTime start,
                                               @Param("end") LocalDateTime end,
                                               @Param("uris") List<String> uris,
