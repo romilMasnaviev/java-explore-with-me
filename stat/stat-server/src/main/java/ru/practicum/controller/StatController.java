d580ba24a6bfa1db54d7cadd8b1a906c2daa6d87
romilMasnaviev <134401753+romilMasnaviev@users.noreply.github.com>
2024-07-17 07:27:44 +0800

    добавлена аннотация @PastOrPresent

--- stat/stat-server/src/main/java/ru/practicum/controller/StatController.java
+++ stat/stat-server/src/main/java/ru/practicum/controller/StatController.java
@@ -13,6 +13,7 @@ import ru.practicum.model.StatEntity;
 import ru.practicum.service.StatService;
 
 import javax.validation.Valid;
+import javax.validation.ValidationException;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -51,6 +52,7 @@ public class StatController {
                                     @RequestParam(name = "uris", required = false) List<String> uris,
                                     @RequestParam(name = "unique", required = false, defaultValue = "false") boolean unique) {
         log.debug("StatController. Get request, Get method, start = {}, end = {}, uris = {}, unique = {}", start, end, uris, unique);
+        if(end.isAfter(LocalDateTime.now())) throw new ValidationException("Время конца в будущем");
         return service.get(start, end, uris, unique);
     }
 
